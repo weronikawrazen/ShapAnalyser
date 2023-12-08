442fd3b57a90a0c0192585cc46f669a0fd6266b6
weronikawrazen <weronikawrazen@gmail.com>
2023-12-08 14:42:04 +0100

    init

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, url_for, request
-from create_reg import Feature
+from flask import Flask, render_template, url_for, request, session
+from create_reg import Feature, generate_data
 # pip3 freeze > requirements.txt 
 #.\venv\Scripts\activate 
 
@@ -17,12 +17,49 @@ def create_app():
             r_seed_DGP = "1"
             if 'r_seed_DGP' in request.form:
                 r_seed_DGP = request.form["r_seed_DGP"]
+                session["r_seed_DGP"] = r_seed_DGP
 
-            if ('x1_dist' and 'x1_min_avg' and 'x1_min_avg' and 'x1_max_var') in request.form:
-                X1 = Feature('x1',request.form["x1_dist"],request.form["x1_min_avg"],request.form["x1_max_var"])
+            if ('x1_dist' and 'x1_min_avg' and 'x1_min_avg' and 'x1_max_var' and 'x1_coeff') in request.form:
+                X1 = Feature('x1',request.form["x1_dist"],request.form["x1_min_avg"],request.form["x1_max_var"],request.form["x1_coeff"])
+                session["X1"] = X1
 
-            return render_template("regression_settings.html", x1_dist = X1.distribution, x1_min_avg = X1.min_avg)    
-    #@app.route('/exchange', methods = ["GET", "POST"])
+            if ('x2_dist' and 'x2_min_avg' and 'x2_min_avg' and 'x2_max_var' and 'x2_coeff') in request.form:
+                X2 = Feature('x2',request.form["x2_dist"],request.form["x2_min_avg"],request.form["x2_max_var"],request.form["x2_coeff"])
+                session["X2"] = X2
+            
+            if ('x3_dist' and 'x3_min_avg' and 'x3_min_avg' and 'x3_max_var' and 'x3_coeff') in request.form:
+                X3 = Feature('x3',request.form["x3_dist"],request.form["x3_min_avg"],request.form["x3_max_var"],request.form["x3_coeff"])
+                session["X3"] = X3                
+            
+            if ('x4_dist' and 'x4_min_avg' and 'x4_min_avg' and 'x4_max_var' and 'x4_coeff') in request.form:
+                X4 = Feature('x4',request.form["x4_dist"],request.form["x4_min_avg"],request.form["x4_max_var"],request.form["x4_coeff"])
+                session["X4"] = X4
+            
+            if ('x5_dist' and 'x5_min_avg' and 'x5_min_avg' and 'x5_max_var' and 'x5_coeff') in request.form:
+                X5 = Feature('x5',request.form["x5_dist"],request.form["x5_min_avg"],request.form["x5_max_var"],request.form["x5_coeff"])
+                session["X5"] = X5 
+
+            if ('x6_dist' and 'x6_min_avg' and 'x6_min_avg' and 'x6_max_var' and 'x6_coeff') in request.form:
+                X6 = Feature('x6',request.form["x6_dist"],request.form["x6_min_avg"],request.form["x6_max_var"],request.form["x6_coeff"])
+                session["X6"] = X6                
+
+            return render_template("genreg.html")    
+    
+    @app.route('/regression', methods = ["GET", "POST"])
+    def create_regression():
+        X1 = session.get("X1")
+        X2 = session.get("X2")
+        X3 = session.get("X3")
+        X4 = session.get("X4")
+        X5 = session.get("X5")
+        X6 = session.get("X6")
+        r_seed_DGP = session.get("r_seed_DGP")
+
+        X1.values, X2.values, X3.values, X4.values, X5.values, X6.values = generate_data(X1,X2,X3,X4,X5,X6, r_seed_DGP)
+
+
+        return "ok"
+
 
     return app
 
--- create_reg.py
+++ create_reg.py
@@ -1,6 +1,74 @@
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator, RegressorMixin, TransformerMixin
+from sklearn.pipeline import make_pipeline
+
 class Feature:
-    def __init__(self, name, distribution, min_avg, max_var):
+    def __init__(self, name, distribution, min_avg, max_var, coeff):
         self.name = name
         self.distribution = distribution
         self.min_avg = min_avg
         self.max_var = max_var
+        self.coeff = coeff
+        self.values= []
+
+class MyOwnTransformer(BaseEstimator, TransformerMixin):
+  def fit(self, X, y=None, alfa = None):
+    return self
+  def transform(self, X):
+     return X
+
+class MyOwnRegressor(BaseEstimator, RegressorMixin):
+  def fit(self, X, y, alfa):
+    return self
+  def predict(self, X, alfa):
+    #print(X.shape)
+    Z = alfa[0] * X[:,0] + alfa[1] * X[:,1] + alfa[2] * X[:,2] + alfa[3] * X[:,3] + alfa[4] * X[:,4] + alfa[5] * X[:,5]
+    return Z
+
+
+
+def generate_data(x1,x2,x3,x4,x5,x6, r_seed_DGP):
+    np.random.seed(r_seed_DGP)
+
+    if x1.distribution == "Normal Distribution":
+        x1.values = np.random.normal(x1.min_avg, x1.max_avg, 10000)
+    elif x1.distribution == "Uniform Distribution":
+        x1.values = np.random.uniform(x1.min_avg, x1.max_avg, 10000)
+
+    if x2.distribution == "Normal Distribution":
+        x2.values = np.random.normal(x2.min_avg, x2.max_avg, 10000)
+    elif x2.distribution == "Uniform Distribution":
+        x2.values = np.random.uniform(x2.min_avg, x2.max_avg, 10000)
+    
+    if x3.distribution == "Normal Distribution":
+        x3.values = np.random.normal(x3.min_avg, x3.max_avg, 10000)
+    elif x3.distribution == "Uniform Distribution":
+        x3.values = np.random.uniform(x3.min_avg, x3.max_avg, 10000)
+
+    if x4.distribution == "Normal Distribution":
+        x4.values = np.random.normal(x4.min_avg, x4.max_avg, 10000)
+    elif x4.distribution == "Uniform Distribution":
+        x4.values = np.random.uniform(x4.min_avg, x4.max_avg, 10000)
+
+    if x5.distribution == "Normal Distribution":
+        x5.values = np.random.normal(x5.min_avg, x5.max_avg, 10000)
+    elif x5.distribution == "Uniform Distribution":
+        x5.values = np.random.uniform(x5.min_avg, x5.max_avg, 10000)
+    
+    if x6.distribution == "Normal Distribution":
+        x6.values = np.random.normal(x6.min_avg, x6.max_avg, 10000)
+    elif x6.distribution == "Uniform Distribution":
+        x6.values = np.random.uniform(x6.min_avg, x6.max_avg, 10000)
+
+    y = x1.coeff * x1.values + x2.coeff * x2.values + x3.coeff * x3.values + x4.coeff * x4.values + x5.coeff * x5.values + x6.coeff * x6.values
+
+    X = np.column_stack((x1.values, x2.values, x3.values, x4.values, x5.values, x6.values))
+
+
+
+    return x1.values, x2.values, x3.values, x4.values, x5.values, x6.values, y
+
+
+
+    
